33282847f8c54f4bdf3fe6c1db052efa29d8ccd6
JimmyCodemunity <jameswafula2002@gmail.com>
2025-06-11 12:18:07 +0300

    bgvideo

---
+++ src/components/BackVideoOverlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BackVideoOverlay = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BackVideoOverlay
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,6 +14,16 @@ const HomePage = () => {
   return (
     <div className="w-full bg-black h-full mx-auto">
       <div className="w-full bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] bg-center bg-cover">
+        <video
+                className="absolute top-0 left-0 w-full h-full object-cover z-0"
+                src="/images/co.mp4"
+                autoPlay
+                loop
+                muted
+                playsInline
+              />
+              {/* Overlay */}
+              <div className="absolute top-0 left-0 w-full h-full bg-black/60 z-10" />
         <NavbarDemo />
         <WelcomeText />
       </div>
@@ -24,6 +34,9 @@ const HomePage = () => {
 
       <GoogleGeminiEffectDemo />
       <ScrollSec />
+      <div className="w-full">
+        
+      </div>
       <Beams />
       <Footer/>
     </div>
